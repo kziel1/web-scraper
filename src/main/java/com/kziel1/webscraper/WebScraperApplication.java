13c0f624c3ecde129f2063ab8d788a24259447df
kziel1 <krzysztofzielinski862@gmail.com>
2018-04-29 13:47:04 +0200

    formatting

--- src/main/java/com/kziel1/webscraper/WebScraperApplication.java
+++ src/main/java/com/kziel1/webscraper/WebScraperApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebScraperApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebScraperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebScraperApplication.class, args);
+    }
 }
